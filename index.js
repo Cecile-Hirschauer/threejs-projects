3ebc7773fa47159e7a193af112262d221ea84190
Cecile-Hirschauer <cecile.hirschauer@bbox.fr>
2024-11-29 14:33:02 +0100

    Define screen size and create renderer

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-console.log('Robot Bobby was here');
+import * as THREE from "three";
+
+// define screen size
+const w = window.innerWidth;
+const h = window.innerHeight;
+
+// Create renderer
+const renderer = new THREE.WebGLRenderer({ antialias: true });
+renderer.setSize(w, h);
+document.body.appendChild(renderer.domElement);

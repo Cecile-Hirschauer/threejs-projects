4e41c013b78ff892392233967e933b92d4ae1c23
Cecile-Hirschauer <cecile.hirschauer@bbox.fr>
2024-11-29 15:57:12 +0100

    Define camera and scene

--- index.js
+++ index.js
@@ -8,3 +8,14 @@ const h = window.innerHeight;
 const renderer = new THREE.WebGLRenderer({ antialias: true });
 renderer.setSize(w, h);
 document.body.appendChild(renderer.domElement);
+
+// Create camera
+const fov = 75;
+const aspect = w / h;
+const near = 0.1;
+const far = 10;
+const camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
+camera.position.z = 2;
+
+// Create scene
+const scene = new THREE.Scene();
